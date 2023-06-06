d238c39c3bfd94162ed419d4504e6443c0d8597d
Jerry <gerardo.alvarez3238@gmail.com>
2023-06-06 08:50:32 -0700

    vertical line implementation

--- saveArray.py
+++ saveArray.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from worlds import *
 
-npzfile = np.load('Jerry_3.npz')
+npzfile = np.load('Oscarc.npz')
 
 x = npzfile['x']
 y1 = np.arange(x.size)
@@ -15,6 +15,7 @@ avg_orientation = np.rad2deg(np.average(abs(y2)))
 
 print("Average Distance from Center:", round(average,3))
 print("Average Orientation:", round(avg_orientation,3), "Degrees")
+plt.axvline(x = center, color = 'b', label = 'axvline - full height')
 plt.plot(x,y1)
 plt.show()
 
